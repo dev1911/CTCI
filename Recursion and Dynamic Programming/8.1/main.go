e7d8f95519e608070c5e765d1d2810ff049004bf
Devansh Shah <43884681+dev1911@users.noreply.github.com>
2020-04-25 16:57:21 +0530

    8.1

---
+++ Recursion and Dynamic Programming/8.1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+)
+
+//For memoization
+var cache map[int]int
+
+func noOfStepsCached(n int , cache map[int]int) int{
+	if _ , prs := cache[n]; prs{
+		return cache[n]
+	}
+	result := noOfStepsCached(n-1 , cache) + noOfStepsCached(n-2 , cache) + noOfStepsCached(n-3 , cache) 
+	cache[n] = result
+	return result
+}
+
+func main(){
+	cache = make(map[int]int)
+	cache[1] = 1
+	cache[2] = 2
+	cache[3] = 3
+	n:=1000
+	fmt.Printf("Number of ways to reach %d steps is %d\n" , n , noOfStepsCached(n , cache))
+}
